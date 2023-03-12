e4e177d88e7a76077b341a6f4948d7c529b31a71
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-03-12 18:15:55 +0300

    feat: add card template

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,33 @@ const popupEdit = document.querySelector('#popupEdit');
 const btnClose = document.querySelector('.btn-close');
 const formElement = document.querySelector('.form');
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // Open modal window (Edit profile)
 btnEdit.addEventListener('click', function (evt) {
   const userName = document.querySelector('.info__name').textContent;
