f254661593adff6f9627bbbfe2ae44f28af01759
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-03-12 17:40:41 +0300

    feat: add saving name and status to page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
 const btnEdit = document.querySelector('.btn-edit');
 const popupEdit = document.querySelector('#popupEdit');
 const btnClose = document.querySelector('.btn-close');
+const formElement = document.querySelector('.form');
 
-// Открытие модального окна (Редактировать профиль)
+// Open modal window (Edit profile)
 btnEdit.addEventListener('click', function (evt) {
+  const userName = document.querySelector('.info__name').textContent;
+  const userStatus = document.querySelector('.info__status').textContent;
+  document.querySelector('#profileName').value = userName;
+  document.querySelector('#profileStatus').value = userStatus;
   popupEdit.classList.add('popup_opened');
 });
 
-// Закрытие модальных окон
+// Closing modal window
+function closePopup (item) {
+  item.target.closest('.popup').classList.remove('popup_opened');
+}
+
+// Close modal window by 'close' button
 btnClose.addEventListener('click', function (evt) {
-  evt.target.closest('.popup').classList.remove('popup_opened');
+  closePopup(evt);
+});
+
+// Set name and status from modal window to page and close modal window
+formElement.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  const inputUserName = document.querySelector('#profileName').value;
+  const inputUserStatus = document.querySelector('#profileStatus').value;
+  document.querySelector('.info__name').textContent = inputUserName;
+  document.querySelector('.info__status').textContent = inputUserStatus;
+  closePopup(evt);
 });
