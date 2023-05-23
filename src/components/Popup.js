743771ba94f77589d3e02f087cf544dd8a8fa8cc
Janiyat Bagandova <janiyatbagandova@yandex.ru>
2023-05-23 21:06:43 +0300

    feat: add Popup class

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,8 +1,32 @@
 export default class Popup {
 
   constructor(selector) {
-    // тело конструктора
+    this._popup = document.querySelector(selector);
   }
 
-  // методы
+  open() {
+    this._popup.classList.add('popup_opened');
+    window.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    window.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.addEventListener('click', function (evt) {
+      if (!evt.target.className.includes('form') &&
+          !evt.target.className.includes('figure') ||
+          evt.target.classList.contains('popup__container')) {
+        this.close();
+      }
+    });
+  }
 }
