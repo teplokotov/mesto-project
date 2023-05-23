567e86b7088ba004ca71d76705a9b00491c79551
Janiyat Bagandova <janiyatbagandova@yandex.ru>
2023-05-23 22:32:31 +0300

    feat: add PopupWithImage class

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,9 +2,16 @@ import Popup from '../components/Popup.js';
 
 export default class PopupWithImage extends Popup {
 
-  constructor() {
-    // тело конструктора
+  constructor(selector) {
+    super(selector);
+    this._popupImage = this._popup.querySelector(".figure__image");
+    this._popupCaption = this._popup.querySelector(".figure__caption");
   }
 
-  // методы
+  open(imageData) {
+    super.open();
+    this._popupImage.src = imageData.link;
+    this._popupImage.alt = imageData.name;
+    this._popupCaption.textContent = imageData.name;
+  }
 }
